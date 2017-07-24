433cfdf0468782ef4816d39db3ade6f4f0fdd331
HamsterJam <hamsterjam99@gmail.com>
2017-07-24 17:33:28 +0900

    Moved Lua functions to their own file

---
+++ include/luaFunctions.h
@@ -0,0 +1,36 @@
+#ifndef HARP_LUA_FUNCTIONS_H
+#define HARP_LUA_FUNCTIONS_H
+
+extern "C" {
+#include <lua.h>
+}
+
+int l_createEntity(lua_State* L);
+int l_deleteEntity(lua_State* L);
+int l_setComponent(lua_State* L);
+int l_setFlag(lua_State* L);
+int l_setParent(lua_State* L);
+
+int l_Vec2Double(lua_State* L);
+int l_Sprite(lua_State* L);
+int l_Shader(lua_State* L);
+int l_FontRenderer(lua_State* L);
+
+int l_SpriteSpec(lua_State* L);
+int l_RectangleSpec(lua_State* L);
+int l_RoundedRectangleSpec(lua_State* L);
+int l_ElipseArcSpec(lua_State* L);
+int l_TriangleSpec(lua_State* L);
+int l_LineSpec(lua_State* L);
+int l_GlyphSpec(lua_State* L);
+int l_TextSpec(lua_State* L);
+
+int l_print(lua_State* L);
+int l_exit(lua_State* L);
+
+int l_callSpriteDestructor(lua_State* L);
+int l_callShaderDestructor(lua_State* L);
+int l_callPrimitiveRendererDestructor(lua_State* L);
+int l_callFontRendererDestructor(lua_State* L);
+
+#endif
