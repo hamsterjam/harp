cba1e39887a0741712ca7fe8d79a86568dcf83e6
Callum Nicholson <hamsterjam99@gmail.com>
2017-11-09 09:45:18 +0900

    Added in the missing color functions (Lua)

--- script/harp.lua
+++ script/harp.lua
@@ -1,12 +1,12 @@
 
 -- More VisualSpec getter functions
 
-function RectangleFillSpec(x, y, w, h, color, ...)
-    return RectangleSpec(x, y, w, h, 0, color, ...)
+function RectFillSpec(x, y, w, h, color, ...)
+    return RectSpec(x, y, w, h, 0, color, ...)
 end
 
-function RoundedRectangleFillSpec(x, y, w, h, r, color, ...)
-    return RoundedRectangleSpec(x, y, w, h, r, 0, color, ...)
+function RoundedRectFillSpec(x, y, w, h, r, color, ...)
+    return RoundedRectSpec(x, y, w, h, r, 0, color, ...)
 end
 
 function CircleFillSpec(x, y, r, color, ...)
@@ -43,16 +43,92 @@ end
 
 -- Color helper functions
 
-function rgbaToColor(r, g, b, a)
-    local color = {}
-    color.r = r
-    color.g = g
-    color.b = b
-    color.a = a
+(function()
+    function rgbaToColor(r, g, b, a)
+        local color = {}
+        color.r = r
+        color.g = g
+        color.b = b
+        color.a = a
 
-    return color
-end
+        return color
+    end
 
-function rgbToColor(r, g, b)
-    return rgbaToColor(r, g, b, 1)
-end
+    function rgbToColor(r, g, b)
+        return rgbaToColor(r, g, b, 1)
+    end
+
+    local function chromaToColor(hh, c, x, m, a)
+        local ret = {
+            r = 0,
+            g = 0,
+            b = 0,
+            a = a
+        }
+
+        if hh < 1 then
+            ret.r = c
+            ret.g = x
+            ret.b = 0
+        elseif hh < 2 then
+            ret.r = x
+            ret.g = c
+            ret.b = 0
+        elseif hh < 3 then
+            ret.r = 0
+            ret.g = c
+            ret.b = x
+        elseif hh < 4 then
+            ret.r = 0
+            ret.g = x
+            ret.b = c
+        elseif hh < 5 then
+            ret.r = x
+            ret.g = 0
+            ret.b = c
+        elseif hh < 6 then
+            ret.r = c
+            ret.g = 0
+            ret.b = x
+        else
+            ret.r = 0
+            ret.g = 0
+            ret.b = 0
+        end
+
+        ret.r = ret.r + m
+        ret.g = ret.g + m
+        ret.b = ret.b + m
+
+        return ret
+    end
+
+    function hsvaToColor(h, s, v, a)
+        h = h % 360
+        local hh = h / 60
+        local c  = v * s
+        local x  = c * (1 - math.abs(hh % 2 - 1))
+        local m  = v - c
+
+        return chromaToColor(hh, c, x, m, a)
+    end
+
+    function hsvToColor(h, s, v)
+        return hsvaToColor(h, s, v, 1.0)
+    end
+
+    function hslaToColor(h, s, l, a)
+        h = h % 360
+        local hh = h / 60
+        local c  = (1 - math.abs(2*l - 1)) * s
+        local x  = c * (1 - math.abs(hh % 2 - 1))
+        local m  = l - c / 2
+
+        return chromaToColor(hh, c, x, m, a)
+    end
+
+    function hslToColor(h, s, l)
+        return hslaToColor(h, s, l, 1.0)
+    end
+
+end)()
