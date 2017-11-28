784f1f4c58416a2df7280fd0b9b1b5159fe97f1b
Callum Nicholson <hamsterjam99@gmail.com>
2017-11-28 15:31:33 +0900

    Made a more sophisticated move function

--- config.lua
+++ config.lua
@@ -21,14 +21,28 @@ primShader = {
 -- This is the kind of thing that you should do in external files...
 -- It's probably best to keep the config file as "value, pair" as possible
 function moveMiku(e, input)
-    local newAcc = {0, 0};
+    local tarVel = {0, 0};
 
-    if input.up    then newAcc[2] = newAcc[2] + 20 end
-    if input.down  then newAcc[2] = newAcc[2] - 20 end
-    if input.left  then newAcc[1] = newAcc[1] - 20 end
-    if input.right then newAcc[1] = newAcc[1] + 20 end
+    if input.up    then tarVel[2] = tarVel[2] + 100 end
+    if input.down  then tarVel[2] = tarVel[2] - 100 end
+    if input.left  then tarVel[1] = tarVel[1] - 100 end
+    if input.right then tarVel[1] = tarVel[1] + 100 end
 
-    e:set(comp.acceleration, Vec2(newAcc))
+    local curVel = asVec2(e:get(comp.velocity))
+
+    local acc = {0, 0}
+    for i = 1, 2 do
+        if math.abs(curVel[i] - tarVel[i]) < 0.0001 then
+            curVel[i] = tarVel[i]
+        elseif tarVel[i] > curVel[i] then
+            acc[i] =  200
+        elseif tarVel[i] < curVel[i] then
+            acc[i] = -200
+        end
+    end
+
+    e:set(comp.velocity, Vec2(curVel))
+    e:set(comp.acceleration, Vec2(acc))
 end
 
 init = (function()
